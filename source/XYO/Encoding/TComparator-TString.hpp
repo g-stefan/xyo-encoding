f153083cf55fae5dee270c643e992d70a02072db
Grigore Stefan <g_stefan@yahoo.com>
2023-04-02 04:02:15 +0300

    Update

---
+++ source/XYO/Encoding/TComparator-TString.hpp
@@ -0,0 +1,50 @@
+// Encoding
+// Copyright (c) 2016-2023 Grigore Stefan <g_stefan@yahoo.com>
+// MIT License (MIT) <http://opensource.org/licenses/MIT>
+// SPDX-FileCopyrightText: 2016-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-License-Identifier: MIT
+
+#ifndef XYO_ENCODING_TCOMPARATOR_TSTRING_HPP
+#define XYO_ENCODING_TCOMPARATOR_TSTRING_HPP
+
+#ifndef XYO_ENCODING_TSTRING_HPP
+#	include <XYO/Encoding/TString.hpp>
+#endif
+
+namespace XYO::ManagedMemory {
+	template <typename U>
+	struct TComparator<XYO::Encoding::TString<U>> {
+			typedef XYO::Encoding::TString<U> T;
+
+			static inline bool isEqual(const T &x, const T &y) {
+				return const_cast<T &>(x) == const_cast<T &>(y);
+			};
+
+			static inline bool isNotEqual(const T &x, const T &y) {
+				return const_cast<T &>(x) != const_cast<T &>(y);
+			};
+
+			static inline bool isLess(const T &x, const T &y) {
+				return const_cast<T &>(x) < const_cast<T &>(y);
+			};
+
+			static inline bool isGreater(const T &x, const T &y) {
+				return const_cast<T &>(x) > const_cast<T &>(y);
+			};
+
+			static inline bool isLessOrEqual(const T &x, const T &y) {
+				return const_cast<T &>(x) <= const_cast<T &>(y);
+			};
+
+			static inline bool isGreaterOrEqual(const T &x, const T &y) {
+				return const_cast<T &>(x) >= const_cast<T &>(y);
+			};
+
+			static inline int compare(const T &x, const T &y) {
+				return (const_cast<T &>(x)).compare(const_cast<T &>(y));
+			};
+	};
+
+};
+
+#endif
--- source/XYO/Encoding/TString.hpp
+++ source/XYO/Encoding/TString.hpp
@@ -324,42 +324,13 @@ namespace XYO::Encoding {
 };
 
 namespace XYO::ManagedMemory {
-	template <typename U>
-	struct TComparator<XYO::Encoding::TString<U>> {
-			typedef XYO::Encoding::TString<U> T;
-
-			static inline bool isEqual(const T &x, const T &y) {
-				return const_cast<T &>(x) == const_cast<T &>(y);
-			};
-
-			static inline bool isNotEqual(const T &x, const T &y) {
-				return const_cast<T &>(x) != const_cast<T &>(y);
-			};
-
-			static inline bool isLess(const T &x, const T &y) {
-				return const_cast<T &>(x) < const_cast<T &>(y);
-			};
-
-			static inline bool isGreater(const T &x, const T &y) {
-				return const_cast<T &>(x) > const_cast<T &>(y);
-			};
-
-			static inline bool isLessOrEqual(const T &x, const T &y) {
-				return const_cast<T &>(x) <= const_cast<T &>(y);
-			};
-
-			static inline bool isGreaterOrEqual(const T &x, const T &y) {
-				return const_cast<T &>(x) >= const_cast<T &>(y);
-			};
-
-			static inline int compare(const T &x, const T &y) {
-				return (const_cast<T &>(x)).compare(const_cast<T &>(y));
-			};
-	};
-
 	template <typename T>
 	struct TMemory<XYO::Encoding::TString<T>> : TMemoryPoolActive<XYO::Encoding::TString<T>> {
 	};
 };
+
+#ifndef XYO_ENCODING_TCOMPARATOR_TSTRING_HPP
+#	include <XYO/Encoding/TComparator-TString.hpp>
+#endif
 
 #endif
